47f0794d29b7ede6e57e8bb658cc8c2ef716b405
Scott Hall <scotthall127@gmail.com>
2018-07-17 01:50:20 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,14 @@
 def prime?(n)
-  array = (2..n-1).to_a
-  array.each do |number|
-    if n % number == 0
-      return false
-    else
-      true
+  if n < 2
+    return false
+  else
+    array = (2..n-1).to_a
+    array.each do |number|
+      if n % number == 0
+        return false
+      else
+        true
+      end
     end
   end
   return true
